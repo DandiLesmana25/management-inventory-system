0eb0c6ef63542e9e952610e95bcaff78ead8e5f9
AgungTriatna <105510687+AgungTriatna@users.noreply.github.com>
2022-12-21 01:11:19 +0700

    fitur pencarian

--- app/views/member/cari-barang.php
+++ app/views/member/cari-barang.php
@@ -36,57 +36,42 @@
                 </form>
 
                 <tr>
-                    <th>No</th>
                     <th>Kode Barang</th>
                     <th>Nama Barang</th>
                     <th>Tipe Barang</th>
                     <th>Jumlah Stok</th>
                     <th>Lokasi</th>
                     <th>Tahun Registrasi</th>
-                    <th>Aksi</th>
                 </tr>
             </thead>
 
-            <?php
-
-            if (isset($_POST['cari_barang'])) {
-                for ($i = 0; $i <= 10; $i++) {
-                    if (
-                        utf8_encode(($data['data_barang'][$i]['kode_barang'])) == $_POST['keywords']
-                        or utf8_encode(($data['data_barang'][$i]['nama_barang'])) == $_POST['keywords']
-                    ) {
-                        echo (utf8_encode(($data['data_barang'][$i]['kode_barang'])));
-                    }
-                }
-            }
-            ?>
 
             <tbody>
-                <?php if ($data['data_barang'] != []) : ?>
-                    <?php $i = 1; ?>
-                    <?php foreach ($data['data_barang'] as $brg) : ?>
-                        <tr>
-                            <td><?= $i ?></td>
-                            <td><?= $brg['kode_barang'] ?></td>
-                            <td><?= $brg['nama_barang'] ?></td>
-                            <td><?= $brg['tipe_barang'] ?></td>
-                            <td><?= $brg['jmlh_stok'] ?></td>
-                            <td><?= $brg['lokasi'] ?></td>
-                            <td><?= $brg['tgl_regist'] ?></td>
-                            <td class="text-center">
-                                <a class="badge badge-info" href="<?= BASEURL ?>/admin/detail-barang/<?= $brg['id'] ?>">Detail</a>
-                                <a class="badge badge-warning" href="<?= BASEURL ?>/admin/ubah-barang/<?= $brg['id'] ?>">Ubah</a>
-                                <a class="badge badge-danger" href="<?= BASEURL ?>/admin/hapus-barang/<?= $brg['id'] ?>" onclick="return confirm('Apakah anda yakin ingin menghapus?')">Hapus</a>
-                            </td>
-                        </tr>
-                        <?php $i++; ?>
-                    <?php endforeach; ?>
-                <?php else : ?>
-                    <tr>
-                        <td colspan="5"><strong>Tidak ada data barang</strong></td>
-                    </tr>
+
+                <?php if (isset($_POST['cari_barang'])) : ?>
+                    <?php for ($i = 0; $i <= 10; $i++) : ?>
+                        <?php if (
+                            strtolower(
+                                utf8_encode(
+                                    ($data['data_barang'][$i]['nama_barang'])
+                                )
+                            ) == strtolower($_POST['keywords'])
+                        ) : ?>
+                            <td><?= utf8_encode(($data['data_barang'][$i]['kode_barang'])) ?></td>
+                            <td><?= utf8_encode(($data['data_barang'][$i]['nama_barang'])) ?></td>
+                            <td><?= utf8_encode(($data['data_barang'][$i]['tipe_barang'])) ?></td>
+                            <td><?= utf8_encode(($data['data_barang'][$i]['jmlh_stok'])) ?></td>
+                            <td><?= utf8_encode(($data['data_barang'][$i]['lokasi'])) ?></td>
+                            <td><?= utf8_encode(($data['data_barang'][$i]['tgl_regist'])) ?></td>
+                            </tr>
+                        <?php endif; ?>
+                    <?php endfor; ?>
                 <?php endif; ?>
+
             </tbody>
+
         </table>
+
+
     </div>
 </div>
