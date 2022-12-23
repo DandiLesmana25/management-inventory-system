bf0a16f95dd277b6b53facc577c6627fa1723668
AgungTriatna <105510687+AgungTriatna@users.noreply.github.com>
2022-12-23 12:43:21 +0700

    update customer service

--- app/views/member/kontak.php
+++ app/views/member/kontak.php
@@ -1,20 +1,125 @@
 <h3>Hubungi Kami</h3>
 <hr>
-<div class="card shadow">
-    <div class="card-body">
-        <p>Jika mengalami Kendala silahkan hubungi kami.</p>
-        <b>
-            <p>Whatsapp</p>
-        </b>
-        <p>081983733783</p>
-        <b>
-            <p>Email</p>
-        </b>
-        <p>email@perusahaan.com</p>
-        <b>
-            <p>Telp / Fax</p>
-        </b>
-        <p>(0361)88432222</p>
-        <p>Terima kasih telah menggunakan layanan Apps perusahaan !</p>
-    </div>
-</div>
+
+<main>
+    <div class="row justify-content-center">
+        <div class="col-10 col-lg-10 col-xl-12 mx-auto">
+            <div class="row mt-4">
+
+                <div class="col-md-3">
+
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                    <span class="circle circle-md bg-primary">
+                                        <span class="fe fe-mail fe-24 text-white"></span>
+                                    </span>
+                                </div>
+                                <div class="col">
+                                    <h3 class="h6 mb-0 text-uppercase">E-Mail</h3>
+                                    <p class="small text-muted mb-0">admin@advics-min.co.id</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="col-md-3">
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                    <span class="circle circle-md bg-success">
+                                        <span class="fe fe-phone fe-24 text-white"></span>
+                                    </span>
+                                </div>
+                                <div class="col">
+                                    <h3 class="h6 mb-0 text-uppercase">Phone</h3>
+                                    <p class="small text-muted mb-0">021-222-222</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3">
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                    <span class="circle circle-md bg-warning">
+                                        <span class="fe fe-message-circle fe-24 text-white"></span>
+                                    </span>
+                                </div>
+                                <div class="col">
+                                    <h3 class="h6 mb-0 text-uppercase">Message</h3>
+                                    <p class="small text-muted mb-0">0838-2424-000</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-3 text-center">
+                                    <span class="circle circle-md bg-secondary">
+                                        <span class="fe fe-twitter fe-24 text-white"></span>
+                                    </span>
+                                </div>
+                                <div class="col">
+                                    <h3 class="h6 mb-0 text-uppercase">Twitter</h3>
+                                    <p class="small text-muted mb-0">Returning item</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end section -->
+
+            <div class="my-5 p-5">
+                <div class="text-center">
+                    <h2 class="mb-0">Knowledge Base</h2>
+                    <p class="lead text-muted mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="row my-5">
+                    <div class="col-md-4">
+                        <h3 class="h5 mt-4 mb-1">Getting start</h3>
+                        <p class="text-muted mb-4">Start working with theme</p>
+                        <ul class="list-unstyled">
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Lorem ipsum dolor sit amet</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Consectetur adipiscing elit</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Integer molestie lorem at massa</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Facilisis in pretium nisl aliquet</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Nulla volutpat aliquam velit</li>
+                        </ul>
+                    </div> <!-- .col-md-->
+                    <div class="col-md-4">
+                        <h3 class="h5 mt-4 mb-1">Integrations</h3>
+                        <p class="text-muted mb-4">How to integrate the theme?</p>
+                        <ul class="list-unstyled">
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Lorem ipsum dolor sit amet</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Consectetur adipiscing elit</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Integer molestie lorem at massa</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Facilisis in pretium nisl aliquet</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Nulla volutpat aliquam velit</li>
+                        </ul>
+                    </div> <!-- .col-md-->
+                    <div class="col-md-4">
+                        <h3 class="h5 mt-4 mb-1">Reporting</h3>
+                        <p class="text-muted mb-4">Report a bug or request a feature</p>
+                        <ul class="list-unstyled">
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Lorem ipsum dolor sit amet</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Consectetur adipiscing elit</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Integer molestie lorem at massa</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Facilisis in pretium nisl aliquet</li>
+                            <li class="my-1"><i class="fe fe-file-text mr-2 text-muted"></i>Nulla volutpat aliquam velit</li>
+                        </ul>
+                    </div> <!-- .col-md-->
+                </div> <!-- .row -->
+            </div </main> <!-- main -->
